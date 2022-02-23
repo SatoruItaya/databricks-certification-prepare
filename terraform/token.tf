d9d83e94007d9f433fe77ef893f5a87ade468ee6
SatoruItaya <str.itaya@gmail.com>
2022-02-23 12:49:06 +0900

    create token

---
+++ terraform/token.tf
@@ -0,0 +1,14 @@
+resource "databricks_token" "pat" {
+  provider         = databricks.created_workspace
+  comment          = "Terraform Provisioning"
+  lifetime_seconds = 86400
+
+  provisioner "local-exec" {
+    command = "echo ${self.token_value} > token"
+  }
+}
+
+output "databricks_token" {
+  value     = databricks_token.pat.token_value
+  sensitive = true
+}
