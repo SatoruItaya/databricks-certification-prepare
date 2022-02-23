421ee12ad19b42f3f4e15b036232ffeda8798a4c
SatoruItaya <str.itaya@gmail.com>
2022-02-23 12:48:50 +0900

    add databricks provider for token

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,3 +5,8 @@ provider "azurerm" {
 provider "databricks" {
   azure_workspace_resource_id = azurerm_databricks_workspace.dbcert.id
 }
+
+provider "databricks" {
+  alias = "created_workspace"
+  host  = azurerm_databricks_workspace.dbcert.workspace_url
+}
