71908942d81688fe8de427479bf94a12f688ab4c
SatoruItaya <str.itaya@gmail.com>
2022-02-23 12:50:51 +0900

    create cli authentication

---
+++ terraform/cli.tf
@@ -0,0 +1,9 @@
+resource "null_resource" "databricks_configure" {
+  triggers = {
+    token = databricks_token.pat.token_value
+  }
+
+  provisioner "local-exec" {
+    command = "databricks configure --host https://${azurerm_databricks_workspace.dbcert.workspace_url}/?o=${azurerm_databricks_workspace.dbcert.workspace_id} --token-file token"
+  }
+}
