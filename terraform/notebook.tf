89b45bfc9e4d73c1e1025c558c2961bd806f9005
SatoruItaya <str.itaya@gmail.com>
2022-02-26 10:20:06 +0900

    add dependency

--- terraform/notebook.tf
+++ terraform/notebook.tf
@@ -1,7 +1,14 @@
 data "databricks_current_user" "me" {
+  depends_on = [
+    azurerm_databricks_workspace.dbcert
+  ]
 }
 
 resource "databricks_notebook" "dbcert" {
   source = "Databricks+Certified+Apache+Spark+Developer.dbc"
   path   = "${data.databricks_current_user.me.home}/Databricks Certified Apache Spark Developer"
+
+  lifecycle {
+    ignore_changes = [format]
+  }
 }
