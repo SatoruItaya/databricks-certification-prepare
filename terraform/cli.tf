8f8c7753600d297bb06825f352fe2469257054f9
SatoruItaya <str.itaya@gmail.com>
2022-02-26 10:56:32 +0900

    add copy retail db resource

--- terraform/cli.tf
+++ terraform/cli.tf
@@ -7,3 +7,14 @@ resource "null_resource" "configure" {
     command = "databricks configure --host https://${azurerm_databricks_workspace.dbcert.workspace_url}/?o=${azurerm_databricks_workspace.dbcert.workspace_id} --token-file token"
   }
 }
+
+# copy the retail_db folder
+resource "null_resource" "copy_retail_db" {
+  triggers = {
+    token = null_resource.configure
+  }
+
+  provisioner "local-exec" {
+    command = "databricks fs cp ../../../dgadiraju/retail_db dbfs:/public/retail_db --recursive"
+  }
+}
